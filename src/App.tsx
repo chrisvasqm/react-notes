e610fe84e89765d926205ea50573d84734ffdc4a
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-18 00:45:30 -0400

    Display notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Button } from '@chakra-ui/react';
 import './App.css';
 import NoteCard from './components/NoteCard';
+import notesData from './resources/notes.json';
 
 function App() {
+  const notes = notesData;
+
+  if (notes.length == 0) return <p>No notes to show.</p>;
+
   return (
     <>
       <h1>Todos App</h1>
       <Button colorScheme='blue'>Click me!</Button>
-      <NoteCard title='Title' body='Body' />
+
+      {notes &&
+        notes.map(note => <NoteCard key={note.title} title={note.title} body={note.body} />)}
     </>
   );
 }
