579eca08ea794f17071d950d0a08a3ec2d2f607a
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-17 20:31:25 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Todos App</h1>
+      <Button colorScheme='blue'>Click me!</Button>
+    </>
+  );
+}
+
+export default App;
